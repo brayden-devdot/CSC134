0ea78f9a6f1edf7e90485416fa6a0b361f86801d
brayden-devdot <jacksonb5420@student.faytechcc.edu>
2026-03-10 18:54:51 +0000

    done

---
+++ m4t1all/m4t1pt1.cpp
@@ -0,0 +1,19 @@
+// CSC 134
+// M4T1
+// Brayden
+// 3/10/2026
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = 0;
+
+    while (number < 5)
+    {
+        cout << "Hello\n";
+        number++;
+    }
+    cout << "that's all!\n";
+    return 0;
+}
